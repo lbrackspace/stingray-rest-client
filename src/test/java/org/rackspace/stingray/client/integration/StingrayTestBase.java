c2cd44ee268582f8c00b6024c5b9f64dd1c6d805
Trevor <trevor.vardeman@rackspace.com>
2013-06-10 14:32:45 -0500

    Finished Traffic IP Integration tests and added a string to the testBase that will serve as a basic name for testing purposes

--- src/test/java/org/rackspace/stingray/client/integration/StingrayTestBase.java
+++ src/test/java/org/rackspace/stingray/client/integration/StingrayTestBase.java
@@ -2,4 +2,6 @@ package org.rackspace.stingray.client.integration;
 
 public class StingrayTestBase {
 
+    public String testname = "i_test";
+
 }
--- src/test/java/org/rackspace/stingray/client/integration/TrafficIpITest.java
+++ src/test/java/org/rackspace/stingray/client/integration/TrafficIpITest.java
@@ -8,13 +8,30 @@ import org.rackspace.stingray.client.exception.StingrayRestClientException;
 import org.rackspace.stingray.client.list.Child;
 import org.rackspace.stingray.client.list.Children;
 import org.rackspace.stingray.client.traffic.ip.TrafficIp;
+import org.rackspace.stingray.client.traffic.ip.TrafficIpBasic;
+import org.rackspace.stingray.client.traffic.ip.TrafficIpProperties;
 
-public class TrafficIpITest {
+public class TrafficIpITest extends StingrayTestBase {
     StingrayRestClient client;
+    TrafficIp tip;
+    TrafficIpProperties properties;
+    TrafficIpBasic basic;
 
     @Before
     public void standUp() {
         client = new StingrayRestClient();
+        basic = new TrafficIpBasic();
+        properties = new TrafficIpProperties();
+        properties.setBasic(basic);
+        tip = new TrafficIp();
+        tip.setProperties(properties);
+    }
+
+    @Test
+    public void testCreateTrafficIp() throws StingrayRestClientException {
+        client.createTrafficIp(testname, tip);
+        TrafficIp createdTip = client.getTrafficIp(testname);
+        Assert.assertNotNull(createdTip);
     }
 
     /**
@@ -22,19 +39,31 @@ public class TrafficIpITest {
      *
      */
     @Test
-    public void getListOfTrafficIps() throws StingrayRestClientException {
+    public void testGetListOfTrafficIps() throws StingrayRestClientException {
         Children children = client.getTrafficIps();
         Assert.assertTrue(children.getChildren().size() > 0);
     }
 
     @Test
-    public void getSpecificTrafficIp() throws StingrayRestClientException {
+    public void testGetTrafficIp() throws StingrayRestClientException {
         Children children = client.getTrafficIps();
         Assert.assertTrue(children.getChildren().size() > 0);
         Child child = children.getChildren().get(0);
         String vsname = child.getName();
         TrafficIp trafficIp = client.getTrafficIp(vsname);
         Assert.assertNotNull(trafficIp);
+    }
 
+    @Test
+    public void testDeleteTrafficIp() throws StingrayRestClientException {
+        client.deleteTrafficIp(testname);
+        Children children = client.getTrafficIps();
+        Boolean deleted = true;
+        for (Child child : children.getChildren()) {
+            if (child.getName().equals(testname)) {
+                deleted = false;
+            }
+        }
+        Assert.assertTrue(deleted);
     }
 }
